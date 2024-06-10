7d411e6a88f71afbbfe3880f36a7d9840d75c632
John Doak <jdoak@ElephantInTheRoom.local>
2024-06-10 11:29:02 -0700

    Provides a standard(non-concurrent) filter, a watchlist reader and the routing_test.go I should have included in the last PR

---
+++ internal/readers/apiserver/watchlist/filter/filter.go
@@ -0,0 +1,50 @@
+// Package filter provides the data types needed to implement a filter cache
+// for filtering out events that are older than the current version.
+package filter
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// Object represent an object that can be filtered.
+type Object interface {
+	GetUID() types.UID
+	GetResourceVersion() string
+	GetCreationTimestamp() metav1.Time
+}
+
+// Item represents the needed fields to filter an object.
+// Field aligned for storage reduction.
+// Also time is stored as unix nano to reduce storage.
+type Item struct {
+	// ResourceVersion is the resource version of the object.
+	ResourceVersion string
+	// LastUpdate is the last time the item was updated in unix nano.
+	LastUpdate int64
+	// Creation is the creation time of the object in unix nano.
+	Creation int64
+}
+
+// IsNewer returns true if the item is newer than the cacheable object.
+func (i Item) IsNewer(c Object) bool {
+	if i.ResourceVersion == c.GetResourceVersion() {
+		return false
+	}
+	if i.Creation > c.GetCreationTimestamp().UnixNano() {
+		return false
+	}
+	return true
+}
+
+// New creates a new Item from a Cacheable object.
+func New(o Object) Item {
+	i := Item{
+		ResourceVersion: o.GetResourceVersion(),
+		Creation:        o.GetCreationTimestamp().Time.UnixNano(),
+		LastUpdate:      time.Now().UnixNano(),
+	}
+	return i
+}
---
+++ internal/readers/apiserver/watchlist/filter/standard/standard.go
@@ -0,0 +1,108 @@
+// Package standard provides a filter cache that stores the latest version information for objects
+// using a map with no locks.
+package standard
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Azure/tattler/internal/readers/apiserver/watchlist/filter"
+
+	"github.com/gostdlib/concurrency/prim/wait"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+// Filter provides a data filter cache for filtering out events that are older than the current version.
+type Filter struct {
+	in  chan watch.Event
+	out chan watch.Event
+	m   map[types.UID]filter.Item
+}
+
+// Option is a opional argument for New().
+type Option func(*Filter) error
+
+// WithSized sets the initial size of the map.
+func WithSized(size int) Option {
+	return func(c *Filter) error {
+		if size < 0 {
+			return errors.New("size must be greater than 0")
+		}
+		c.m = make(map[types.UID]filter.Item, size)
+		return nil
+	}
+}
+
+// New creates a new Filter. Closing the input channel will stop the Filter.
+func New(ctx context.Context, in, out chan watch.Event, options ...Option) (*Filter, error) {
+	ctx = context.WithoutCancel(ctx)
+
+	cc := &Filter{
+		in:  in,
+		out: out,
+	}
+
+	for _, o := range options {
+		if err := o(cc); err != nil {
+			return nil, err
+		}
+	}
+	if cc.m == nil {
+		cc.m = map[types.UID]filter.Item{}
+	}
+
+	g := wait.Group{}
+
+	g.Go(ctx, func(ctx context.Context) error {
+		cc.run()
+		return nil
+	})
+
+	go func() {
+		g.Wait(ctx)
+		close(cc.out)
+	}()
+
+	return cc, nil
+}
+
+// run is the main loop of the Checker.
+func (c *Filter) run() {
+	for event := range c.in {
+		c.handleEvent(event)
+	}
+}
+
+// handleEvent looks at the event type and acts accordingly.
+func (c *Filter) handleEvent(event watch.Event) {
+	obj := event.Object.(filter.Object)
+	if event.Type == watch.Deleted {
+		delete(c.m, obj.GetUID())
+		c.out <- event
+		return
+	}
+
+	if c.setMapItem(obj) {
+		c.out <- event
+	}
+}
+
+// setMapItem sets the item in the map if it doesn't exist or if the new pod is newer.
+// Returns true if the item was set, false if the item was not set.
+func (c *Filter) setMapItem(newItem filter.Object) (cached bool) {
+	oldItem, ok := c.m[newItem.GetUID()]
+	if !ok {
+		c.m[newItem.GetUID()] = filter.New(newItem)
+		return true
+	}
+
+	// If the new item is older than the cached item, we don't need to update the cache.
+	if oldItem.IsNewer(newItem) {
+		return false
+	}
+
+	// We need to update the item in the map.
+	c.m[newItem.GetUID()] = filter.New(newItem)
+	return true
+}
