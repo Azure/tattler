fb1e9ecd5ad11d94fd6bf587cb23c6b0e7f84d64
shanalily <shanalily@gmail.com>
2024-08-21 13:02:07 -0700

    buckets

--- metrics/batching/metrics.go
+++ metrics/batching/metrics.go
@@ -43,9 +43,7 @@ func Init(meter api.Meter) error {
 	batchAgeSeconds, err = meter.Float64Histogram(
 		metricName("batch_age_seconds"),
 		api.WithDescription("age of batch when emitted"),
-		api.WithExplicitBucketBoundaries(64, 128, 256, 512, 1024, 2048, 4096),
-		// 	Buckets: []float64{0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3,
-		// 		4, 5, 6, 8, 10, 15, 20, 30, 45, 60},
+		api.WithExplicitBucketBoundaries(0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3, 4, 5),
 	)
 	return nil
 }
